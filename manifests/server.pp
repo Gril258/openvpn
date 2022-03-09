f114c48303ce982c8cd2ac4d15f76dc06dacee9d
Martin Krutina <gril51043@gmail.com>
2022-03-09 11:24:16 +0100

    update

--- manifests/server.pp
+++ manifests/server.pp
@@ -21,6 +21,7 @@ define openvpn::server (
     $push_route = [],
     $script_security = '3',
     $cipher = 'BF-CBC',
+    $status_log_path = "openvpn-status-${name}.log",
   ){
   # directory for certificate authority called ca_name
   $ca_path = "/etc/openvpn/ca/${ca_name}/easy-rsa"
